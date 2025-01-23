d1748b5b06917237b2d77c88eb25e445185572eb
obuzietter <obuyacalvince672@gmail.com>
2025-01-23 18:37:53 +0300

    Refactor alumni carousel layout and update avatar images
    Improves the layout of the alumni carousel by refactoring the code and updating the avatar images to use local assets instead of external URLs. This ensures consistent rendering and improves performance.

--- resources/views/alumni/alumni_carousel.blade.php
+++ resources/views/alumni/alumni_carousel.blade.php
@@ -5,20 +5,19 @@
         </div>
         <div class="row">
             <!-- Carousel wrapper -->
-            <div id="testimonialCarousel" class="carousel slide text-center carousel-dark"
-                data-bs-ride="carousel">
+            <div id="testimonialCarousel" class="carousel slide text-center carousel-dark" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img class="rounded-circle shadow-1-strong mb-4"
-                        src="https://img.freepik.com/free-psd/middle-age-black-man-portrait-isolated_23-2151884915.jpg?uid=R97350360&ga=GA1.1.569336961.1721632028&semt=ais_incoming" alt="avatar"
-                        style="width: 150px; height: 150px; object-fit:cover" />
+                        <img class="rounded-circle shadow-1-strong mb-4" src="{{ asset('/images/weiszer.jpg') }}"
+                            alt="avatar" style="width: 150px; height: 150px; object-fit:cover" />
                         <div class="row d-flex justify-content-center">
                             <div class="col-lg-8">
                                 <h5 class="mb-3">Antony Weiszer</h5>
                                 <p>Full Stack Developer</p>
                                 <p class="text-muted">
                                     <i class="fas fa-quote-left pe-2"></i>
-                                    "From front-end design to back-end systems, I learned it all here. The hands-on training gave me the confidence to launch my career as a full-stack developer."
+                                    "From front-end design to back-end systems, I learned it all here. The hands-on
+                                    training gave me the confidence to launch my career as a full-stack developer."
                                 </p>
                             </div>
                         </div>
@@ -31,16 +30,16 @@
                         </ul>
                     </div>
                     <div class="carousel-item">
-                        <img class="rounded-circle shadow-1-strong mb-4"
-                            src="https://img.freepik.com/free-photo/medium-shot-beautiful-african-girl-posing_23-2151438222.jpg?uid=R97350360&ga=GA1.1.569336961.1721632028&semt=ais_incoming" alt="avatar"
-                            style="width: 150px; height: 150px; object-fit:cover" />
+                        <img class="rounded-circle shadow-1-strong mb-4" src="{{ asset('/images/maggy.jpg') }}"
+                            alt="avatar" style="width: 150px; height: 150px; object-fit:cover" />
                         <div class="row d-flex justify-content-center">
                             <div class="col-lg-8">
                                 <h5 class="mb-3">Margaret W</h5>
                                 <p>Front End Developer</p>
                                 <p class="text-muted">
                                     <i class="fas fa-quote-left pe-2"></i>
-                                    "Thanks to this institution, I mastered HTML, CSS, and JavaScript. Now, I'm building stunning, user-friendly websites for clients worldwide!"
+                                    "Thanks to this institution, I mastered HTML, CSS, and JavaScript. Now, I'm building
+                                    stunning, user-friendly websites for clients worldwide!"
                                 </p>
                             </div>
                         </div>
@@ -53,16 +52,18 @@
                         </ul>
                     </div>
                     <div class="carousel-item">
-                        <img class="rounded-circle shadow-1-strong mb-4"
-                            src="{{ asset('images/chris.jpg') }}" alt="avatar" style="width: 150px; height: 150px; object-fit:cover" />
-                            
+                        <img class="rounded-circle shadow-1-strong mb-4" src="{{ asset('images/chris.jpg') }}"
+                            alt="avatar" style="width: 150px; height: 150px; object-fit:cover" />
+
                         <div class="row d-flex justify-content-center">
                             <div class="col-lg-8">
                                 <h5 class="mb-3">Chris Oketch</h5>
                                 <p>Web3 Developer</p>
                                 <p class="text-muted">
                                     <i class="fas fa-quote-left pe-2"></i>
-                                    "I learnt a lot about making websites from Palmate but I was also introduced to blockchain and decentralized apps. Today, I'm confident in creating the next generation of Web3 solutions!"
+                                    "I learnt a lot about making websites from Palmate but I was also introduced to
+                                    blockchain and decentralized apps. Today, I'm confident in creating the next
+                                    generation of Web3 solutions!"
                                 </p>
                             </div>
                         </div>
@@ -74,7 +75,7 @@
                             <li><i class="far fa-star fa-sm"></i></li>
                         </ul>
                     </div>
-                    
+
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#testimonialCarousel"
                     data-bs-slide="prev">
