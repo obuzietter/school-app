35ed25c289af4bbeb5ee8550ccc88eb48b682361
obuzietter <obuyacalvince672@gmail.com>
2025-02-18 17:54:34 +0300

    Updates brand name to 'Palmate Academy'
    Sets the brand name to 'Palmate Academy' in the Filament CMS panel provider.
    
    Improves branding consistency throughout the application.
    
    Fixes #N/A

--- app/Providers/Filament/CmsPanelProvider.php
+++ app/Providers/Filament/CmsPanelProvider.php
@@ -34,6 +34,7 @@ class CmsPanelProvider extends PanelProvider
                 // 'primary' => Color::Green,
                 'primary' => '#40a798',
             ])
+            ->brandName('Palmate Academy')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
